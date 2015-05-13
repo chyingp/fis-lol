e767c595b740b3c3bfc7224e982f61d9199a15ce
casperchen(陈映平) <chyingp@gmail.com>
2015-05-13 18:11:40 +0800

    main file

---
+++ lol.js
@@ -0,0 +1,6 @@
+var fis = module.exports = require('fis');
+
+fis.name = 'lol';
+fis.require.prefixes = [ 'lol', 'fis' ];
+fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
+fis.cli.help.commands = [ 'release', 'install', 'server', 'init' ];
