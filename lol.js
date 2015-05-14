d3976dfc1892e370c2b233053ed46f3a6e270ed9
casperchen(陈映平) <chyingp@gmail.com>
2015-05-14 09:27:22 +0800

    修改配置

--- lol.js
+++ lol.js
@@ -4,3 +4,94 @@ fis.name = 'lol';
 fis.require.prefixes = [ 'lol', 'fis' ];
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 fis.cli.help.commands = [ 'release', 'install', 'server', 'init' ];
+
+// 配置
+fis.config.merge({
+	project: {
+		exclude: ['tests/**']
+	},
+	roadmap: {		
+		path: [
+            {
+                reg : /^\/modules\/([^\/]+)\/\1\.(js)$/i,
+                isMod : true,
+                id : '$1',
+                release : '$&'
+            },
+            {
+                reg : /^\/modules\/(.*)\.(js)$/i,
+                isMod : true,
+                id : '$1',
+                release : '$&'
+            },
+            {
+                reg : /^\/lego_modules\/([^\/]+)\/(\d+\.\d+\.\d+)\/index\.(js)$/i,
+                isMod : true,
+                id : '$1',
+                release : '$&'
+            }			
+		]
+	},
+	modules: {
+		postprocessor: {
+			js: ['jswrapper', 'require-async', function(content, file, conf){
+                
+                // if(file.subpath.indexOf('/modules/')!=-1){  // 业务组件
+                //     var reg = /require\('([^'"]+)'\)/g;
+                //     var matches = null;
+                //     while( (matches = reg.exec(content)) ){
+                //         !file.extra && (file.extra = {});
+                //         !file.extra.requires && (file.extra.requires = []);
+                //         file.extra.requires.push(matches[1]);
+                //     }                    
+                // }
+
+                return content;
+            }],
+            html: ['require-async', function(content, file, conf){
+                
+                // content = content.replace(/require\.async\((\[[^\[\]]+\])/g, function(matchword, $1){
+                //     console.log($1);
+                //     return matchword;
+                // });
+                return content;
+            }]
+		},
+        postpackager: function(ret, conf, settings, opt){                
+            
+            var alias = {};
+            var projectPath = fis.project.getProjectPath();
+            var subpath = '';
+
+            fis.util.map(ret.src, function (subpath, file) {                
+                
+                if(file.isJsLike && file.isMod && subpath.match(/^\/modules\//) || subpath.match(/^\/lego_modules\//) ){
+                    alias[file.id] = {
+                        url: file.url,
+                        deps: file.requires
+                    };                                        
+                }
+                // console.log(file);
+                
+            });
+
+            fis.util.map(ret.src, function(subpath, file){
+                if(file.isHtmlLike){
+                    var content = file.getContent();
+                    file.setContent(content.replace(/<\/head>/, '<script>require.resourceMap({res:'+ JSON.stringify(alias) +'})</script>$&'));
+                }
+            });
+
+            // console.log(fis.project.getProjectPath());
+        }
+	},
+	settings: {
+		postprocessor : {
+            jswrapper : {
+            	type: 'amd',
+            	wrapAll: false
+                // template : 'try{ ${content} }catch(e){ e.message += "${id}"; throw e; }'
+            }
+        }
+	}
+});
